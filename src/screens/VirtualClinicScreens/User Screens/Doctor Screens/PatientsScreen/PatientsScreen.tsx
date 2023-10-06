b8f49fb950a4b45ce5a6111c7ab99a2772def95c
Deema Magdy <deema2magdy@gmail.com>
2023-10-06 20:14:05 +0300

    getPatients view (unfinished)

--- src/screens/VirtualClinicScreens/User Screens/Doctor Screens/PatientsScreen/PatientsScreen.tsx
+++ src/screens/VirtualClinicScreens/User Screens/Doctor Screens/PatientsScreen/PatientsScreen.tsx
@@ -2,10 +2,29 @@ import styles from "screens/VirtualClinicScreens/User Screens/Doctor Screens/Pat
 import { useNavigate } from "react-router";
 import { useEffect, useState } from "react";
 
+
 const PatientsScreen = () => {
+  const[patients, setPatients] = useState<any[]>([])
+  useEffect(() =>{
+    const fetchPatients = async () =>{
+      const response = await fetch('/doctor/getPatients')
+      const json = await response.json()
+
+      if(response.ok){
+        setPatients(json)
+      }
+    }
+    fetchPatients()
+  }, [])
+
   return (
     <div className={`w-full flex flex-col items-start justify-center`}>
       <h1>Doctor Patients Screen</h1>
+      <div className="patients">
+        {patients && patients.map((patient)=>(
+          <p key={patient['_id']}>{patient}</p>
+        ))}
+      </div>
     </div>
   );
 };

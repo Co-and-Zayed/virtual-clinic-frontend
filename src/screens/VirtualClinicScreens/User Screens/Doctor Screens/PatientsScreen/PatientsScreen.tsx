cff35b136bb351ec8c648376f11894f3b032822d
Neveen Fahmy <127423792+neveenfahmyy@users.noreply.github.com>
2023-10-06 23:01:00 +0300

    Update PatientScreen.tsx

--- src/screens/VirtualClinicScreens/User Screens/Doctor Screens/PatientsScreen/PatientsScreen.tsx
+++ src/screens/VirtualClinicScreens/User Screens/Doctor Screens/PatientsScreen/PatientsScreen.tsx
@@ -7,7 +7,17 @@ const PatientsScreen = () => {
   const[patients, setPatients] = useState<any[]>([])
   useEffect(() =>{
     const fetchPatients = async () =>{
-      const response = await fetch('/doctor/getPatients')
+
+      const email = "seif@gmail.com";
+      const requestBody = JSON.stringify({ doctor: email });
+
+      const response = await fetch('/doctor/getPatients', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: requestBody,
+        })
       const json = await response.json()
 
       if(response.ok){
@@ -17,12 +27,35 @@ const PatientsScreen = () => {
     fetchPatients()
   }, [])
 
+  const[patientInfo, setPatientInfo] = useState<any[]>([])
+  useEffect(() =>{
+    const fetchPatientInfo = async () =>{
+
+      const _id = "";
+      const requestBody = JSON.stringify({ _id: _id });
+
+      const response = await fetch('/doctor/getPatientInfo', {
+          method: 'POST',
+          headers: {
+            'Content-Type': 'application/json',
+          },
+          body: requestBody,
+        })
+      const json = await response.json()
+
+      if(response.ok){
+        setPatientInfo(json)
+      }
+    }
+    fetchPatientInfo()
+  }, [])
+
   return (
     <div className={`w-full flex flex-col items-start justify-center`}>
       <h1>Doctor Patients Screen</h1>
       <div className="patients">
         {patients && patients.map((patient)=>(
-          <p key={patient['_id']}>{patient}</p>
+          <button key={patient['_id']}>{patient.name}</button>
         ))}
       </div>
     </div>

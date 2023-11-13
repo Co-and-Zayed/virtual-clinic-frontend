b3530fa9b93d2f10190a3e7fd46731efc55aefb3
Mostafa Sohile <mostafa.sohile@gmail.com>
2023-11-13 20:16:18 +0200

    Push

--- src/screens/VirtualClinicScreens/User Screens/Doctor Screens/DashboardScreen/DashboardScreen.tsx
+++ src/screens/VirtualClinicScreens/User Screens/Doctor Screens/DashboardScreen/DashboardScreen.tsx
@@ -1,43 +1,28 @@
 import styles from "screens/VirtualClinicScreens/User Screens/Doctor Screens/DashboardScreen/DashboardScreen.module.css";
 import { useNavigate } from "react-router";
-import { useEffect, useRef, useState } from "react";
-import Highlighter from "react-highlight-words";
+import { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { RootState } from "redux/rootReducer";
 import React from "react";
-import {
-  Input,
-  notification,
-  Table,
-  InputRef,
-  Space,
-  Button,
-  Badge,
-} from "antd";
+import type { ColumnsType } from "antd/es/table";
+import { listAllPatientsAction } from "redux/VirtualClinicRedux/ListAllPatients/listAllPatientsAction";
 import { SearchOutlined } from "@ant-design/icons";
-import { listAllContractsAction } from "redux/VirtualClinicRedux/ListAllContracts/listAllContractsAction";
-import type {
-  ColumnType,
-  ColumnsType,
-  FilterValue,
-  SorterResult,
-  FilterConfirmProps,
-} from "antd/es/table/interface";
+import { Table, Input, Button } from "antd";
 import * as Routes from "Routes/VirtualClinicRoutes/paths";
 import { useFunctions } from "hooks/useFunctions";
 //import { listAllUsersAction } from "redux/VirtualClinicRedux/ListAllUsers/listAllUsersAction";
 
 const DashboardScreen = () => {
   const dispatch: any = useDispatch();
-  const { allContracts } = useSelector(
-    (state: RootState) => state.listAllContractsReducer
+  const { patientsLoading, allPatients } = useSelector(
+    (state: RootState) => state.listAllPatientsReducer
   );
 
   const { userData } = useSelector((state: RootState) => state.userReducer);
   const { handleDownload } = useFunctions();
 
   useEffect(() => {
-    dispatch(listAllContractsAction({ doctorUsername: userData?.username })); // sending the request, and update the states
+    dispatch(listAllPatientsAction({ doctorUsername: userData?.username })); // sending the request, and update the states
   }, []);
 
   const generateExpandable = (record: any) => {
@@ -66,119 +51,6 @@ const DashboardScreen = () => {
   const navigate = useNavigate();
   const [selectedRowKey, setSelectedRowKey] = useState<string>("");
   const [expandedRowKeys, setExpandedRowKeys] = useState<string[]>([]);
-
-  const handleContractRequest = async (ID: any, values: any) => {
-    const res = await fetch(
-      `${process.env.REACT_APP_BACKEND_URL}doctor/${values}Contract`,
-      {
-        method: "PUT",
-        headers: {
-          "Content-Type": "application/json",
-          Authorization: `Bearer ${accessToken}`,
-        },
-        body: JSON.stringify({
-          _id: ID,
-        }),
-      }
-    );
-    dispatch(listAllContractsAction());
-    const data = await res.json();
-    notification.success({
-      message: "Contract Status Updated Successfully",
-      placement: "topRight",
-    });
-  };
-
-  const getColumnSearchProps = (
-    dataIndex: DataIndex
-  ): ColumnType<DataType> => ({
-    filterDropdown: ({
-      setSelectedKeys,
-      selectedKeys,
-      confirm,
-      clearFilters,
-      close,
-    }) => (
-      <div style={{ padding: 8 }} onKeyDown={(e) => e.stopPropagation()}>
-        <Input
-          ref={searchInput}
-          placeholder={`Search ${dataIndex}`}
-          value={selectedKeys[0]}
-          onChange={(e) =>
-            setSelectedKeys(e.target.value ? [e.target.value] : [])
-          }
-          onPressEnter={() =>
-            handleSearch(selectedKeys as string[], confirm, dataIndex)
-          }
-          style={{ marginBottom: 8, display: "block" }}
-        />
-        <Space>
-          <Button
-            type="primary"
-            onClick={() =>
-              handleSearch(selectedKeys as string[], confirm, dataIndex)
-            }
-            icon={<SearchOutlined />}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Search
-          </Button>
-          <Button
-            onClick={() => clearFilters && handleReset(clearFilters)}
-            size="small"
-            style={{ width: 90 }}
-          >
-            Reset
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              confirm({ closeDropdown: false });
-              setSearchText((selectedKeys as string[])[0]);
-              setSearchedColumn(dataIndex);
-            }}
-          >
-            Filter
-          </Button>
-          <Button
-            type="link"
-            size="small"
-            onClick={() => {
-              close();
-            }}
-          >
-            close
-          </Button>
-        </Space>
-      </div>
-    ),
-    filterIcon: (filtered: boolean) => (
-      <SearchOutlined style={{ color: filtered ? "#1677ff" : undefined }} />
-    ),
-    onFilter: (value, record) =>
-      record[dataIndex]
-        .toString()
-        .toLowerCase()
-        .includes((value as string).toLowerCase()),
-    onFilterDropdownOpenChange: (visible) => {
-      if (visible) {
-        setTimeout(() => searchInput.current?.select(), 100);
-      }
-    },
-    render: (text) =>
-      searchedColumn === dataIndex ? (
-        <Highlighter
-          highlightStyle={{ backgroundColor: "#ffc069", padding: 0 }}
-          searchWords={[searchText]}
-          autoEscape
-          textToHighlight={text ? text.toString() : ""}
-        />
-      ) : (
-        text
-      ),
-  });
 
   const handleExpand = (expanded: boolean, record: DataType) => {
     const key = record.key.toString();
@@ -214,74 +86,81 @@ const DashboardScreen = () => {
     setSearchText(""); // Clear the search text
     setSearchedColumn(""); // Clear the searched column
   };
-  const getStatusColor = (status: string) => {
-    switch (status) {
-      case "ACCEPTED":
-        return "success";
-      case "PENDING":
-        return "processing";
-      case "REJECTED":
-        return "error";
-      case "EXPIRED":
-        return "warning";
-      default:
-        return "warning";
-    }
-  };
-
-  const getStatusText = (status: string) => {
-    switch (status) {
-      case "ACCEPTED":
-        return "Accepted";
-      case "PENDING":
-        return "Pending";
-      case "REJECTED":
-        return "Rejected";
-      case "EXPIRED":
-        return "Expired";
-      default:
-        return "Pending";
-    }
-  };
 
   interface DataType {
     key: React.Key;
-    doctorUsername: string;
-    date: string;
-    endDate: string;
-    hourlyRate: Number;
-    clinicRate: Number;
-    status: string;
+    name: string;
+    email: string;
+    healthRecords: string;
+    date_of_birth: Date;
+    gender: string;
+    mobileNumber: string;
+    emergencyContactName: string;
+    emergencyContactNumber: string;
   }
 
-  type DataIndex = keyof DataType;
-
-  const data: DataType[] = allContracts?.map((contract: any) => {
-    const formatDate = (dateString: string): string => {
-      const [year, month, day] = dateString.split("T")[0].split("-");
-      return `${day}/${month}/${year}`;
-    };
-    return {
-      doctorUsername: contract.doctorUsername,
-      date: formatDate(contract.date),
-      endDate: formatDate(contract.endDate),
-      hourlyRate: contract.hourlyRate,
-      clinicRate: contract.clinicRate,
-      status: contract.status,
-      key: contract._id,
-    };
-  });
-
   const columns: ColumnsType<DataType> = [
+    Table.SELECTION_COLUMN,
     {
       title: "Name",
-      dataIndex: "doctorUsername",
+      dataIndex: "name",
       key: "name",
-      ...getColumnSearchProps("doctorUsername"),
-      sorter: (a, b) => a.doctorUsername.localeCompare(b.doctorUsername),
-      sortDirections: ["descend", "ascend"],
+      filterDropdown: ({
+        setSelectedKeys,
+        selectedKeys,
+        confirm,
+        clearFilters,
+      }) => (
+        <div style={{ padding: 8 }}>
+          <input
+            placeholder={`Search name`}
+            value={
+              selectedKeys[0] !== undefined ? selectedKeys[0].toString() : ""
+            }
+            onChange={(e) =>
+              setSelectedKeys(e.target.value ? [e.target.value] : [])
+            }
+            onKeyDown={(e) => {
+              if (e.key === "Enter") {
+                handleSearch(selectedKeys, confirm, "name");
+              }
+            }}
+            style={{ width: 188, marginBottom: 8, display: "block" }}
+          />
+          <Button
+            onClick={() => handleSearch(selectedKeys, confirm, "name")}
+            size="small"
+            style={{ width: 90, marginRight: 8 }}
+          >
+            Search
+          </Button>
+          <Button
+            onClick={handleResetSearch}
+            size="small"
+            style={{ width: 90 }}
+          >
+            Clear
+          </Button>
+        </div>
+      ),
+      filterIcon: (filtered) => (
+        <SearchOutlined style={{ color: filtered ? "#1890ff" : undefined }} />
+      ),
+      onFilter: (value, record) => {
+        const stringValue = String(value).toLowerCase();
+        const name = String(record.name).toLowerCase();
+        return name.includes(stringValue);
+      },
+      filtered: !!searchText,
+      render: (text) =>
+        searchedColumn === "name" ? (
+          <span style={{ backgroundColor: "#ffc069" }}>{text}</span>
+        ) : (
+          text
+        ),
     },
-
+    Table.EXPAND_COLUMN,
+    { title: "Email", dataIndex: "email", key: "Email" },
     {
       title: "Date of Birth",
       dataIndex: "date_of_birth",
@@ -295,35 +174,17 @@ const DashboardScreen = () => {
         );
       },
     },
+    { title: "Gender", dataIndex: "gender", key: "gender" },
     {
-      title: "Action",
-      dataIndex: "",
-      key: "delete",
-      render: (record) => (
-        <Space>
-          <Space>
-            <a onClick={() => handleContractRequest(record.key, "accept")}>
-              Accept
-            </a>
-          </Space>
-          <Space>
-            <a onClick={() => handleContractRequest(record.key, "reject")}>
-              Reject
-            </a>
-          </Space>
-        </Space>
-      ),
+      title: "Health Records",
+      dataIndex: "healthRecords",
+      key: "healthRecords",
     },
   ];
 
-  useEffect(() => {
-    console.log("Sending Request");
-    dispatch(listAllContractsAction());
-  }, []);
-
   return (
     <div className={`w-full flex flex-col items-start justify-center`}>
-      <h1>My Contracts</h1>
+      <h1>My Patients</h1>
 
       {
         <a onClick={() => handleDownload({ files: userData?.doctorDocuments })}>
@@ -334,13 +195,29 @@ const DashboardScreen = () => {
       <button
         className={styles.button}
         onClick={() => {
-          navigate(Routes.VIEW_CONTRACT_PATH, {});
+          navigate(Routes.DOCTORS_UPCOMING_PATIENTS_PATH, {});
         }}
       >
         Filter by upcoming appointments
       </button>
 
-      <Table columns={columns} dataSource={data} />
+      <Table
+        columns={columns}
+        rowSelection={{
+          type: "radio",
+          selectedRowKeys: [selectedRowKey],
+          onSelect: (record: DataType) => handleRowSelect(record),
+        }}
+        expandable={{
+          expandedRowRender: (record) => generateExpandable(record),
+          onExpand: (expanded, record) => handleExpand(expanded, record),
+        }}
+        dataSource={allPatients?.map((patient: any, index: any) => ({
+          ...patient,
+          key: patient.key || index.toString(), // Use index as a fallback key
+        }))}
+        expandedRowKeys={expandedRowKeys}
+      />
     </div>
   );
 };
